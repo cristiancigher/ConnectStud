00031029970c15eff6912bde2f5f593aaee3ca4f
cristiancigher <40380404+cristiancigher@users.noreply.github.com>
2018-06-22 00:11:44 +0300

    Delete fb

--- facebook/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');    // DB username
-define('DB_PASSWORD', '');    // DB password
-define('DB_DATABASE', 'siteDAW');      // DB name
-$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
-$database = mysqli_select_db($connection, DB_DATABASE) or die( "Unable to select database");
-?>
